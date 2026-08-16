18d44edcb6071fa133ac01ab275e01499c95fb40
Hidetake Iwata <int128@gmail.com>
2026-08-16 21:08:35 +0900

    Initial implementation

---
+++ src/provider.ts
@@ -0,0 +1,53 @@
+import { setTimeout } from 'node:timers/promises'
+import * as vscode from 'vscode'
+import type { InlineCompletionAgent } from './agent.ts'
+
+export class InlineCompletionItemProvider implements vscode.InlineCompletionItemProvider {
+  private readonly debouncer = new Debouncer(300)
+  private readonly agent
+
+  constructor(agent: InlineCompletionAgent) {
+    this.agent = agent
+  }
+
+  async provideInlineCompletionItems(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    _context: vscode.InlineCompletionContext,
+    cancellationToken: vscode.CancellationToken,
+  ) {
+    const cancellationController = new AbortController()
+    cancellationToken.onCancellationRequested(() => cancellationController.abort())
+    return await this.debouncer.run(cancellationController.signal, async () => {
+      try {
+        return await this.agent.generate(document, position, cancellationController.signal)
+      } catch (error) {
+        vscode.window.showErrorMessage(`Inline completion error: ${error}`)
+        throw error
+      }
+    })
+  }
+}
+
+class Debouncer {
+  private cancelPreviousWait?: () => void
+  private readonly delay
+
+  constructor(delay: number) {
+    this.delay = delay
+  }
+
+  async run<T>(externalSignal: AbortSignal, task: () => Promise<T>): Promise<T> {
+    if (this.cancelPreviousWait) {
+      this.cancelPreviousWait()
+    }
+
+    const currentWaitController = new AbortController()
+    this.cancelPreviousWait = () => currentWaitController.abort()
+    await setTimeout(this.delay, undefined, {
+      signal: AbortSignal.any([currentWaitController.signal, externalSignal]),
+    })
+
+    return await task()
+  }
+}
